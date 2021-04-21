58c50ce959329f17fcb1a8ef28ebec82ee6b8c55
halvywood <75034666+halvywood@users.noreply.github.com>
2021-04-20 23:44:47 -0500

    added code to draw bar graph

--- Static/js/app.js
+++ Static/js/app.js
@@ -1,19 +1,52 @@
 console.log("app.js loaded");
 
 function DrawBargraph(sampleId) {
-    console.log('DrawBargraph(${sampleId})');
+    console.log(`DrawBargraph(${sampleId})`);
+
+    d3.json("data/samples.json").then(data => {
+        // console.log(data);
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+        
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+        
+        yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: "h"
+
+        }
+
+        var barArray = [barData];
+        var barLayout = {
+            titile: "Top 10 Bacteria Cultures Found",
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot("bar", barArray, barLayout);
+
+
+    });
 }
 
 function DrawBubblechart(sampleId) {
-    console.log('DrawBubblechart(${sampleId})');
+    console.log(`DrawBubblechart(${sampleId})`);
 }
 
 function ShowMetadata(sampleId) {
-    console.log('ShowMetadata(${sampleId})');
+    console.log(`ShowMetadata(${sampleId})`);
 }
 
 function optionChanged(newSampleId) {
-    console.log('User selected ${newSampleId}');
+    console.log(`User selected ${newSampleId}`);
 
     DrawBargraph(newSampleId);
     DrawBubblechart(newSampleId);
@@ -46,15 +79,7 @@ function InitDashboard() {
 
     });
 
-    // Update the bargraph
-    // Update the bubblechart
-    // Update the demographic information
+
 }
-
-
-
-
-
-
 
 InitDashboard();
