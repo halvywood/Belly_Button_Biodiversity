e64794ad3f1136a5363b6e74170f2d113d257e22
halvywood <75034666+halvywood@users.noreply.github.com>
2021-04-20 23:08:33 -0500

    Add an event handler and stubs for the functions - office hours code

--- Static/js/app.js
+++ Static/js/app.js
@@ -1,6 +1,24 @@
 console.log("app.js loaded");
 
+function DrawBargraph(sampleId) {
+    console.log('DrawBargraph(${sampleId})');
+}
 
+function DrawBubblechart(sampleId) {
+    console.log('DrawBubblechart(${sampleId})');
+}
+
+function ShowMetadata(sampleId) {
+    console.log('ShowMetadata(${sampleId})');
+}
+
+function optionChanged(newSampleId) {
+    console.log('User selected ${newSampleId}');
+
+    DrawBargraph(newSampleId);
+    DrawBubblechart(newSampleId);
+    ShowMetadata(newSampleId);
+}
 
 function InitDashboard() {
     console.log("InitDashboard()");
@@ -8,8 +26,8 @@ function InitDashboard() {
     // Populate the dropdown
     var selector = d3.select("#selDataset");
 
-    d3.json("data/samples.json").then(data => {
-        console.log(data);
+    d3.json("data/samples.json").then(data =>  {
+        // console.log(data);
 
         var sampleNames = data.names;
 
@@ -19,6 +37,13 @@ function InitDashboard() {
                 .property("value", sampleId);
         
         });
+
+        var id = sampleNames[0];
+
+        DrawBargraph(id);
+        DrawBubblechart(id);
+        ShowMetadata(id);
+
     });
 
     // Update the bargraph
