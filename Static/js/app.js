9c7dae6d7958ec658c8bb0af2333f4fb44f584c6
halvywood <75034666+halvywood@users.noreply.github.com>
2021-04-22 00:12:16 -0500

    added the bubble chart drawing code

--- Static/js/app.js
+++ Static/js/app.js
@@ -1,7 +1,7 @@
 console.log("app.js loaded");
 
 function DrawBargraph(sampleId) {
-    console.log(`DrawBargraph(${sampleId})`);
+    console.log(`Drawbargraph(${sampleId})`);
 
     d3.json("data/samples.json").then(data => {
         // console.log(data);
@@ -13,32 +13,66 @@ function DrawBargraph(sampleId) {
         var otu_ids = result.otu_ids;
         var otu_labels = result.otu_labels;
         var sample_values = result.sample_values;
-        
+
         yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
 
         var barData = {
-            x: sample_values.slice(0, 10).reverse(),
-            y: yticks,
+            x: sample_values.slice(0,10).reverse(),
+            y: yticks, 
             type: "bar",
             text: otu_labels.slice(0, 10).reverse(),
             orientation: "h"
-
         }
 
         var barArray = [barData];
+
         var barLayout = {
-            titile: "Top 10 Bacteria Cultures Found",
+            title: "Top 10 Bacteria Cultures Found",
             margin: {t: 30, l: 150}
         }
 
-        Plotly.newPlot("bar", barArray, barLayout);
+        Plotly.newPlot("bar", barArray, barLayout); 
 
 
     });
 }
 
 function DrawBubblechart(sampleId) {
-    console.log(`DrawBubblechart(${sampleId})`);
+    d3.json("data/samples.json").then((data) => {
+      // @TODO: Build a Bubble Chart Using the Sample Data
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+      
+      
+        var bubbleLayout = {
+            margin: { t: 0 },
+            hovermode: "closests",
+            xaxis: { title: "OTU ID"}
+        }
+  
+        var bubbleData = [
+        {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        }
+      ]
+
+      Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+  
+    });
+
 }
 
 function ShowMetadata(sampleId) {
@@ -51,7 +85,10 @@ function optionChanged(newSampleId) {
     DrawBargraph(newSampleId);
     DrawBubblechart(newSampleId);
     ShowMetadata(newSampleId);
+
 }
+
+
 
 function InitDashboard() {
     console.log("InitDashboard()");
@@ -59,7 +96,7 @@ function InitDashboard() {
     // Populate the dropdown
     var selector = d3.select("#selDataset");
 
-    d3.json("data/samples.json").then(data =>  {
+    d3.json("data/samples.json").then(data => {
         // console.log(data);
 
         var sampleNames = data.names;
@@ -68,7 +105,6 @@ function InitDashboard() {
             selector.append("option")
                 .text(sampleId)
                 .property("value", sampleId);
-        
         });
 
         var id = sampleNames[0];
@@ -78,8 +114,6 @@ function InitDashboard() {
         ShowMetadata(id);
 
     });
-
-
 }
 
 InitDashboard();
